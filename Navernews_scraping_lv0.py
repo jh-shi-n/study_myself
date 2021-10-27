4b914a29b538cd10382c9e44a5ef75039aaccb4b
Shin_JH <61272439+shinjh-dl@users.noreply.github.com>
2021-10-27 13:16:35 +0900

    Update Navernews_scraping_lv0.py

--- Navernews_scraping_lv0.py
+++ Navernews_scraping_lv0.py
@@ -16,4 +16,41 @@ for k in article:
     title = k.select_one('a.news_tit').text
     print(title)
    
-#네이버 검색창에 Search_value 입력값을 검색 후, 결과로 나오는 뉴스 결과 1페이지 정보를 얻어옴
+#네이버 검색창에 Search_value 입력값을 검색 후, 결과로 나오는 뉴스 결과 1페이지 기사 Title을 얻어옴
+
+
+#-----------------------------------------------------------------------------------------------------------
+
+import requests
+import pandas as pd
+import numpy as np
+from bs4 import BeautifulSoup
+
+#검색어 입력 (string 입력)
+search_value = input(str())
+sort_value = input()
+# 0 = 관련성, 1= 최신, 2=오래된순
+
+# 기본설정
+result = pd.DataFrame()
+page_number = 1
+
+#range 값 1 증가마다 10개씩 더 긁어옴
+for time in range(0,4):
+    
+    url = f"https://search.naver.com/search.naver?where=news&sm=tab_pge&query={search_value}&sort={sort_value}&photo=0&field=0&pd=0&ds=&de=&mynews=1&office_type=1&office_section_code=2&news_office_checked=1001&nso=so:dd,p:all,a:all&start={page_number}"
+    headers = {'user-agent':'Mozilla/5.0'}
+    raw = requests.get(url, headers=headers)    
+    html = BeautifulSoup(raw.text, 'lxml')
+    
+    #기사 정보
+    news_title = [title['title'] for title in html.find_all('a', attrs={'class':'news_tit'})]
+    news_url = [url['href'] for url in html.find_all('a', attrs={'class':'news_tit'})]
+    news_date = [date.get_text() for date in html.find_all('span', attrs={'class':'info'})]
+    
+    df = pd.DataFrame({'기사제목' : news_title, '기사작성' : news_date, "기사주소" : news_url})
+    result = pd.concat([result, df])
+    
+    page_number += 10
+
+result
